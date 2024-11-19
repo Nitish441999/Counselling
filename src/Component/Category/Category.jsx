7ff9d3718dbd317269189c47e0b9c76ba09a6b80
Nitish Kumar <nitish441999@gmail.com>
2024-11-19 16:32:36 +0530

    some change

--- src/Component/Category/Category.jsx
+++ src/Component/Category/Category.jsx
@@ -31,19 +31,19 @@ function Category() {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 4,
         },
       },
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
         },
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
         },
       },
     ],
